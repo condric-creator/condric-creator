5b5180fb86144842e51a4af89f267d4b3f16373d
condric-creator <emmayancondric@gmail.com>
2024-11-08 03:08:08 -0800

    Add files via upload

---
+++ scores/scores.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+ 
+ int main(){
+	 int scores[4][2]={65,92,35,70,84,72,59,67};
+	 int i;
+	 int j;
+	 
+	 	
+	 
+	 
+for(i=0;i<4;i++){
+	for(j=0;j<2;j++){
+		printf("%d",scores[i][j]);
+	}
+	printf("\n");
+	
+}
+return 0;	 	 
+ }
